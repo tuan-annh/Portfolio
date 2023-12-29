614df7e776bdb4943ced3d1809f4e056b437991a
tuan-annh <tuananh4102001@gmail.com>
2023-12-29 09:45:16 +0700

    update

---
+++ components/AboutPageComponent.tsx
@@ -0,0 +1,71 @@
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function AboutComponent() {
+  return (
+    <div
+      style={{ backgroundImage: "url(/bg-3.jpg)" }}
+      className="h-screen flex md:flex-row flex-col md:justify-center items-center w-screen bg-cover bg-center gap-12 md:gap-20 px-5"
+    >
+      <div className="relative mt-10 md:mt-0">
+        <Image
+          src="/image-about.png"
+          alt="image-about"
+          width={400}
+          height={400}
+          className="object-contain relative z-10 hidden md:block"
+        />
+        <Image
+          src="/image-about.png"
+          alt="image-about"
+          width={400}
+          height={400}
+          className="object-contain relative z-10 block mx-auto md:hidden w-1/2"
+        />
+        <div className="absolute animate-ping rounded-full w-[250px] h-[250px] border-8 bottom-[60%] right-[60%]"></div>
+        <div className="absolute animate-ping rounded-full w-[250px] h-[250px] border-8 bottom-[60%] left-[60%]"></div>
+        <div className="absolute animate-ping rounded-full w-[250px] h-[250px] border-8 top-[60%] left-[60%]"></div>
+        <div className="absolute animate-ping rounded-full w-[250px] h-[250px] border-8 top-[60%] right-[60%]"></div>
+      </div>
+      <div className="text-white gap-3 flex flex-col justify-center items-center md:items-start text-xl md:text-2xl">
+        <span>Xin chào, mình là Đinh Tuấn Anh </span>
+        <span>
+          <span className="font-bold">Ngày sinh: </span>04-10-2001
+        </span>
+        <span>
+          <span className="font-bold">Giới tính: </span>Nam
+        </span>
+        <span>
+          <span className="font-bold">Tốt nghiệp: </span>Trường đại học Công
+          Nghiệp Hà Nội
+        </span>
+        <span>
+          <span className="font-bold">Nghề nghiệp: </span>Lập trình viên Web
+        </span>
+        <Link
+          href="/DinhTuanAnh_Intern_Front-End.pdf"
+          className="flex justify-center items-center gap-2 px-6 mt-5 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700"
+          download={true}
+          target="_blank"
+        >
+          Download CV
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+            />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ components/SkillComponent.tsx
@@ -0,0 +1,67 @@
+"use client"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { SkillData } from "@/constants"
+import "swiper/css"
+import { Autoplay } from "swiper/modules"
+import Image from "next/image"
+
+export default function MySkillComponent() {
+  return (
+    <div
+      style={{ backgroundImage: "url(/bg-2.jpg)" }}
+      className="h-screen flex w-screen justify-center bg-cover bg-center"
+    >
+      <div className="flex flex-col gap-20 max-w-[80%] text-center items-center mt-[10%]">
+        <span className="text-white font-semibold text-[50px] animate-bounce">
+          Skills{" "}
+          <span className="text-[50px] text-center font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            &
+          </span>{" "}
+          Technologies
+        </span>
+        <Swiper
+          slidesPerView={5}
+          loop={true}
+          autoplay={{ delay: 0, disableOnInteraction: false }}
+          speed={4000}
+          modules={[Autoplay]}
+          className="max-w-[95%] md:max-w-[70%]"
+        >
+          {SkillData.map((skill, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                alt={skill.name}
+                src={skill.Image}
+                width={skill.width}
+                height={skill.height}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Swiper
+          slidesPerView={5}
+          loop={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+            reverseDirection: true,
+          }}
+          speed={4000}
+          modules={[Autoplay]}
+          className="max-w-[95%] md:max-w-[70%]"
+        >
+          {SkillData.map((skill, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                alt={skill.name}
+                src={skill.Image}
+                width={skill.width}
+                height={skill.height}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  )
+}
